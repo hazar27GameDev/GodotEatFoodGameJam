26c72dcb8c5609d16df1c7f42d3f06e6bf5b06c6
hazar27GameDev <harrison.imre@gmail.com>
2025-04-05 13:31:27 +1100

    lean in direction

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+@onready var sprite = $Sprite2D
 
 const SPEED = 100.0
 const JUMP_VELOCITY = -400.0
@@ -19,10 +20,20 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	if Input.is_action_pressed("left"):
+		sprite.skew = -0.5
+	else:
+		if Input.is_action_just_released("right"):
+			sprite.skew = 0
+	if Input.is_action_pressed("right"):
+		sprite.skew = 0.5
+	else:
+		if Input.is_action_just_released("left"):
+			sprite.skew = 0
 
 	move_and_slide()
