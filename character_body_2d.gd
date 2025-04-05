b2d94747d17139233e95616e1ac583d057e615bf
hazar27GameDev <harrison.imre@gmail.com>
2025-04-05 12:46:25 +1100

    spriteadded

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,8 +1,10 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 100.0
 const JUMP_VELOCITY = -400.0
+
+var frog = false
 
 
 func _physics_process(delta: float) -> void:
@@ -12,7 +14,8 @@ func _physics_process(delta: float) -> void:
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		if frog == true:
+			velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
